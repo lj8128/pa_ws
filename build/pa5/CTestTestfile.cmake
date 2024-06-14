c42bd35c457f1ea1e997047f61a47909221a445e
James <leejs8128@gmail.com>
2024-06-14 21:17:20 +0000

    Finalize first draft of the solution to the simulation part of PA5

---
+++ build/pa5/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /my_ros_data/pa_ws/src/pa5
+# Build directory: /my_ros_data/pa_ws/build/pa5
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
