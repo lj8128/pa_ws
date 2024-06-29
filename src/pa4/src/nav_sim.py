85eaf1812f011c1dc19e37b18bd4c24164c0de1a
James <leejs8128@gmail.com>
2024-06-29 17:53:10 +0000

    Extend the face-fiducial and move-to-fiducial functionalities so that, for any robot-fiducial pair (r,f), r can face and move to f as long as there are no obstacles between r and f.

--- src/pa4/src/nav_sim.py
+++ src/pa4/src/nav_sim.py
@@ -22,7 +22,7 @@ class NavSim:
         # The value D such that, when the distance between the robot's base_link
         # frame's origin and the origin of the pin it's travelling towards
         # equals D meters, the robot should stop.
-        self.HALT_DIST = 0.2 
+        self.HALT_DIST = 0.3 
         
     def odom_cb(self, msg):
         """Callback function for `odom_sub`."""
@@ -71,10 +71,12 @@ class NavSim:
         self.cmd_vel_pub.publish(self.twist)
 
     def scan_for_fids(self):
+        rospy.loginfo(f'---> Scanning for fiducials...')
         target_yaw = math.pi * 2 * (9 / 10)
         self.turn_to_heading(target_yaw, 0.5)
 
     def match_pin_rotation(self, pin_id):
+        rospy.loginfo(f'---> Matching orientation of pin_{pin_id}...') 
         pin_tf = self.tf_buffer.lookup_transform(
             f'pin_{pin_id}',
             f'pin_{pin_id}',
@@ -90,14 +92,44 @@ class NavSim:
         self.turn_to_heading(yaw, 0.2) 
 
     def face_pin(self, pin_id):
+        rospy.loginfo(f'---> Facing pin_{pin_id}...') 
         base_to_pin_transl = self.tf_buffer.lookup_transform(
             f'base_link',
             f'pin_{pin_id}',
             rospy.Time()
         ).transform.translation
-        yaw = math.atan(
-            base_to_pin_transl.y / base_to_pin_transl.x
-            )
+
+        x_tr = base_to_pin_transl.x
+        y_tr = base_to_pin_transl.y
+        yaw = 0.0
+
+        x_tr_abs = abs(x_tr)
+        y_tr_abs = abs(y_tr)
+
+        if not y_tr == 0:
+            if x_tr > 0 and y_tr > 0:
+                rospy.loginfo('x_tr: + // y_tr: +') 
+                yaw = math.atan(y_tr_abs / x_tr_abs)
+            elif x_tr > 0 and y_tr < 0:
+                rospy.loginfo('x_tr: + // y_tr: -') 
+                yaw = 2 * math.pi - math.atan(y_tr_abs / x_tr_abs)
+            elif x_tr < 0 and y_tr < 0:
+                rospy.loginfo('x_tr: - // y_tr: -') 
+                yaw = math.pi + math.atan(y_tr_abs / x_tr_abs)
+            elif x_tr < 0 and y_tr > 0:
+                rospy.loginfo('x_tr: - // y_tr: +') 
+                yaw = math.pi - math.atan(y_tr_abs / x_tr_abs)
+            elif y_tr > 0:
+                rospy.loginfo("x_tr: 0 // y_tr: +")
+                yaw = math.pi / 2
+            else:
+                rospy.loginfo("x_tr: 0 // y_tr: -")
+                yaw = 2 * math.pi - (math.pi / 2)
+        elif x_tr < 0:
+            rospy.loginfo("x_tr: - // y_tr: 0")
+            yaw = math.pi
+
+        rospy.loginfo(f'yaw: {yaw}')
         self.turn_to_heading(yaw, 0.2)
 
     def get_dist_base_to_pin(self, pin_id):
@@ -118,14 +150,10 @@ class NavSim:
         )
 
     def move_to_pin(self, pin_id):
+        rospy.loginfo(f'---> Moving to pin_{pin_id}...')
         # Reset distance traveled
         self.traveled_dist = 0.0
-        #-----------REFACTOR--------------------
-        target_dist = self.get_dist_base_to_pin(pin_id)
-        rospy.loginfo(f'raw_target_dist= {target_dist}')
-        target_dist -= self.HALT_DIST
-        rospy.loginfo(f'target_dist = {target_dist}')
-        #-------------------------------
+        target_dist = self.get_dist_base_to_pin(pin_id) - self.HALT_DIST
 
         rate = rospy.Rate(10)
 
@@ -133,7 +161,6 @@ class NavSim:
             not rospy.is_shutdown() and 
             not math.isclose(self.traveled_dist, target_dist, abs_tol=0.05)
         ):
-            rospy.loginfo(f'traveled_dist = {self.traveled_dist}')
             self.twist.linear.x = 0.05
             self.cmd_vel_pub.publish(self.twist)
             rate.sleep()
@@ -151,13 +178,12 @@ class NavSim:
                     rospy.Time()
                     ).transform.translation
                 rospy.loginfo(f'base_to_pin_{pin_id}_transl.x: {base_to_pin_transl.x}')
-                rospy.loginfo(f'base_to_pin_{pin_id}_transl.y: {base_to_pin_transl.x}')
+                rospy.loginfo(f'base_to_pin_{pin_id}_transl.y: {base_to_pin_transl.y}')
             except (
                 tf2_ros.LookupException,
                 tf2_ros.ExtrapolationException,
                 tf2_ros.ConnectivityException
                 ):
-                rospy.loginfo("tf2_ros exception triggered!")
                 continue
             rate.sleep()
 
@@ -166,7 +192,8 @@ if __name__ == '__main__':
 
     nav = NavSim()
     nav.scan_for_fids()
-    nav.match_pin_rotation(0)
-    nav.face_pin(0)
-    nav.move_to_pin(0)
-    # nav.print_transl(0)
+    target_pin_ids = [0, 1, 2, 3]
+    for pin_id in target_pin_ids:
+        nav.match_pin_rotation(pin_id)
+        nav.face_pin(pin_id)
+        nav.move_to_pin(pin_id)
